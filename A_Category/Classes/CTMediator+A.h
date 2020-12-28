5e9e4d441c9f07ae6e8a2b96e640cb83ea3c0515
H <lihe@koolearn.com>
2020-12-28 15:03:17 +0800

    first commit

---
+++ A_Category/Classes/CTMediator+A.h
@@ -0,0 +1,16 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by 李贺 on 2020/12/28.
+//
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (A)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ A_Category/Classes/CTMediator+A.m
@@ -0,0 +1,12 @@
+//
+//  CTMediator+A.m
+//  A_Category
+//
+//  Created by 李贺 on 2020/12/28.
+//
+
+#import "CTMediator+A.h"
+
+@implementation CTMediator (A)
+
+@end
